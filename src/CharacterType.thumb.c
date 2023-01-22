e2382413bdc421d7f56d1f23d712ea62c72d736f
unknown <bry_of_the_sky@yahoo.com>
2023-01-22 18:15:22 +0800

    Remove unused files

--- src/CharacterType.thumb.c
+++ src/CharacterType.thumb.c
@@ -20,8 +20,8 @@ void kankandara_setCrawlCharacter(CharacterAttr* character);
 void windowmask_init(CharacterAttr* character, ControlTypePool* controlPool);
 void windowmask_setCharacter(CharacterAttr* character);
 
-const FuncCharacterInit chacterInit[] = { NULL, NULL, &ghostlyHand_init, &ghostMan_init, NULL, &werewolf_init, &crow_init, &zombie_init,
+const FuncCharacterInit chacterInit[] = { NULL, NULL, /*&ghostlyHand_init, &ghostMan_init, NULL, &werewolf_init, &crow_init, &zombie_init,*/
 	&kankandara_init, &kankandara_crawlInit, &windowmask_init};
-const FuncCharacterSet characterSet[] = { NULL, NULL, &ghostlyHand_setCharacter, &ghostMan_setCharacter, NULL, 
-	&werewolf_setCharacter,  &crow_setCharacter, &zombie_setCharacter, &kankandara_setCharacter, &kankandara_setCrawlCharacter,
+const FuncCharacterSet characterSet[] = { NULL, NULL, /*&ghostlyHand_setCharacter, &ghostMan_setCharacter, NULL, 
+	&werewolf_setCharacter,  &crow_setCharacter, &zombie_setCharacter, */&kankandara_setCharacter, &kankandara_setCrawlCharacter,
 	&windowmask_setCharacter};
